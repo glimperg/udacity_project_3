f14e8a78bbae82c73a0b5a0590958a2d9b8b0fe6
Gido Limperg <gido@daisys.ai>
2022-12-27 16:36:01 +0100

    Added model training code with tests

--- starter/train_model.py
+++ starter/train_model.py
@@ -1,10 +1,14 @@
 # Script to train machine learning model.
-
+import joblib
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+from ml.data import process_data
+from ml.model import train_model
 
 # Add code to load in the data.
+data = pd.read_csv("data/census.csv")
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -20,9 +24,24 @@ cat_features = [
     "native-country",
 ]
 X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
+    train,
+    categorical_features=cat_features,
+    label="salary",
+    training=True
 )
 
-# Proces the test data with the process_data function.
+# Process the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test,
+    categorical_features=cat_features,
+    label="salary",
+    training=False,
+    encoder=encoder,
+    lb=lb
+)
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+joblib.dump(model, "model/model.pkl")
+joblib.dump(encoder, "model/encoder.pkl")
+print("Model trained successfully, saved model and encoder in model directory.")
---
+++ tests/test_ml_model.py
@@ -0,0 +1,71 @@
+import pytest
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+from starter.ml.data import process_data
+from starter.ml.model import train_model, compute_model_metrics, inference
+
+
+@pytest.fixture
+def dataset():
+    """
+    Toy dataset to test ML methods.
+    """
+    df = pd.DataFrame(
+        {
+            "a": [0, 1, 2, 3],
+            "b": ["w", "x", "y", "z"],
+            "salary": [">50K", "<=50K", "<=50K", ">50K"]
+        }
+    )
+    train, test = train_test_split(df)
+    X_train, y_train, encoder, lb = process_data(
+        train,
+        categorical_features=["a", "b"],
+        label="salary",
+        training=True
+    )
+    X_test, y_test, encoder, lb = process_data(
+        test,
+        categorical_features=["a", "b"],
+        label="salary",
+        training=False,
+        encoder=encoder,
+        lb=lb
+    )
+    return X_train, X_test, y_train, y_test
+
+
+def test_train_model(dataset):
+    """
+    Test if the model trains correctly on the toy dataset.
+    """
+    X_train, X_test, y_train, y_test = dataset
+    model = train_model(X_train, y_train)
+    assert isinstance(model, RandomForestClassifier), "Output model is not a RandomForestClassifier"
+
+
+def test_inference(dataset):
+    """
+    Test running inference using a trained model.
+    """
+    X_train, X_test, y_train, y_test = dataset
+    model = train_model(X_train, y_train)
+    preds = inference(model, X_test)
+    assert isinstance(preds, np.ndarray), "Model inference output is not a numpy array"
+    assert len(X_test) == len(preds), "Prediction output size differs from input data size"
+
+
+def test_compute_model_metrics(dataset):
+    """
+    Test model metrics computation by comparing model predictions to the test labels.
+    """
+    X_train, X_test, y_train, y_test = dataset
+    model = train_model(X_train, y_train)
+    preds = inference(model, X_test)    
+    precision, recall, fbeta = compute_model_metrics(y_test, preds)
+    assert isinstance(precision, float), "Output precision is not a float"
+    assert isinstance(recall, float), "Output recall is not a float"
+    assert isinstance(fbeta, float), "Output fbeta is not a float"
